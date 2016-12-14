0b22ef6672b4f8bff97419b59aac1a58ce6328e5
Ngô Xuân Bách <thanbaiks@gmail.com>
2016-12-14 17:57:35 +0700

    Init

--- PrologExample/Form1.cs
+++ PrologExample/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SbsSW.SwiPlCs;
 
 namespace PrologExample
 {
@@ -15,6 +16,25 @@ namespace PrologExample
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Environment.SetEnvironmentVariable("SWI_HOME_DIR", @"C:\Program Files (x86)\swipl");
+            Environment.SetEnvironmentVariable("Path", Environment.GetEnvironmentVariable("Path") + @";C:\Program Files (x86)\swipl\bin");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] args = { "-q" };
+            PlEngine.Initialize(args);
+            using (var query = new PlQuery("member(X, [ngo, xuan, bach])."))
+            {
+                foreach (var sol in query.SolutionVariables)
+                {
+                    MessageBox.Show(sol["X"].ToString());
+                }
+            }
         }
     }
 }
